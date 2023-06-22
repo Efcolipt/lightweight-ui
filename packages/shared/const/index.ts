01abd1c69af70742c74500316c57cb7458df7050
Ivan Baykov <efcolipt@gmail.com>
2023-06-22 23:57:11 +0300

    chore: update packages deps

--- packages/shared/const/index.ts
+++ packages/shared/const/index.ts
@@ -2,7 +2,7 @@ const PROJECT_NAME = '@lightweight-ui'
 
 const createAlias = (alias: string) => `${PROJECT_NAME}/${alias}`
 
-export const ALIASES = {
-  shared: createAlias('shared'),
+export const BUILD_LIB_ALIASES = {
   core: createAlias('core'),
+  shared: createAlias('shared'),
 }
--- packages/ui/core/vite.config.ts
+++ packages/ui/core/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
-import { ALIASES } from '@lightweight-ui/shared'
+import { BUILD_LIB_ALIASES } from '@lightweight-ui/shared'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  optimizeDeps: {
-    include: [ALIASES.shared],
-  },
-
   plugins: [
     dts({
       insertTypesEntry: true,
@@ -17,7 +13,7 @@ export default defineConfig({
 
   build: {
     lib: {
-      name: ALIASES.core,
+      name: BUILD_LIB_ALIASES.core,
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['cjs', 'es', 'umd'],
       fileName: (format, entryName) => `${entryName}.${format}.js`,
